8089113317e4ea14342323819fd27fc273401cb1
Tim Kelly <tkelly3118@gmail.com>
2026-06-05 20:41:16 +0100

    updated CORs

--- src/main/java/com/tim/tvschedule/application/web/controller/ProgramController.java
+++ src/main/java/com/tim/tvschedule/application/web/controller/ProgramController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/programs")
 public class ProgramController {
