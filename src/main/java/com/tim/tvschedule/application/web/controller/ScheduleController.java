98b89131e0c9b22fa10a77790e1f0e2857e62737
Tim Kelly <tkelly3118@gmail.com>
2026-06-05 20:26:48 +0100

    open CORs for testing

--- src/main/java/com/tim/tvschedule/application/web/controller/ScheduleController.java
+++ src/main/java/com/tim/tvschedule/application/web/controller/ScheduleController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/schedules")
 public class ScheduleController {
